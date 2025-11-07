16d97edc857d50878ab43bf33d6a41828bf40cae
Alex Weng <alexweng600@gmail.com>
2025-11-06 22:38:37 -0500

    change to domain api

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import { MsgRecord, PagedResult, Template } from "../types";
 
-const API_BASE = "/api"; // proxied backend base path
+const API_BASE = "http://localhost:8082";
 const DEFAULT_TRACE_ID = "mytrace123";
 const DEFAULT_USER_ID = "77";
 
